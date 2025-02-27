73456e51debdf4b578e39c78765273c15dd0e91d
Durga Shree N <82532967+durgashree15@users.noreply.github.com>
2025-02-26 22:06:37 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -42,7 +42,7 @@ def process_data(df):
     # Drop the 'descriptors' column if you don't need it anymore
     df = df.drop('descriptors', axis=1)
 
-    model=joblib.load("gb_imp.pkl")
+    model=joblib.load("RET_Gradient_Boosting2.pkl")
 
     Bioactivity = model.predict(df.drop("Ligand SMILES", axis=1))
 
